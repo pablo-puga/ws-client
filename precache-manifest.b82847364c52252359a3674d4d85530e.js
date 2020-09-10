413ad7754ccd46ab60ae2fe013586b5fb2d4590c
Pablo Puga <pablo.puga.git@gmail.com>
2020-09-10 20:09:50 +0200

    Updates

---
+++ precache-manifest.b82847364c52252359a3674d4d85530e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3edb91f4936e9cab9b98e68af0f733",
+    "url": "/ws-client/index.html"
+  },
+  {
+    "revision": "d1acdbdc93d63fbafee1",
+    "url": "/ws-client/static/css/main.270ef53e.chunk.css"
+  },
+  {
+    "revision": "ff1af93f05f5d240e2fc",
+    "url": "/ws-client/static/js/2.18c69014.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ws-client/static/js/2.18c69014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1acdbdc93d63fbafee1",
+    "url": "/ws-client/static/js/main.f32cf44a.chunk.js"
+  },
+  {
+    "revision": "1262b60c2fc1e9de76ac",
+    "url": "/ws-client/static/js/runtime-main.b5f4dbda.js"
+  },
+  {
+    "revision": "be6fe705b7d36b6f56f84a3c35a4be50",
+    "url": "/ws-client/static/media/under-construction.be6fe705.jpg"
+  }
+]);
